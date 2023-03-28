09c6d6c784978df4bd1e4cd418aee79f013c0ae8
Linchenn <linchan@google.com>
2023-03-28 10:10:32 -0700

    tst

--- depth-estimation/demos/3d_photo/js/depth.js
+++ depth-estimation/demos/3d_photo/js/depth.js
@@ -36,7 +36,7 @@ const IMAGE_PREFIX = 'im';
  */
 function changeImage(file, image_id) {
   const reader = new FileReader();
-  reader.onload = function(e) {
+  reader.onload = function (e) {
     const img = document.createElement('img');
     img.src = e.target.result
 
@@ -67,14 +67,14 @@ function changeImage(file, image_id) {
         const context = resizeCanvas.getContext('2d');
         context.clearRect(0, 0, WIDTH, HEIGHT);
         context.drawImage(
-            img, event.detail.x, event.detail.y, event.detail.width,
-            event.detail.height, 0, 0, WIDTH, HEIGHT);
+          img, event.detail.x, event.detail.y, event.detail.width,
+          event.detail.height, 0, 0, WIDTH, HEIGHT);
 
         const changeImg = document.getElementById(image_id);
         changeImg.src = resizeCanvas.toDataURL();
         changeImg.width = WIDTH;
         changeImg.height = HEIGHT;
-        changeImg.onload = function() {
+        changeImg.onload = function () {
           predict();
         }
       },
@@ -91,7 +91,7 @@ function changeImage(file, image_id) {
 function loadImage(filename, element_id) {
   let _img = document.getElementById(element_id);
   let newImg = new Image;
-  newImg.onload = function() {
+  newImg.onload = function () {
     _img.src = this.src;
     predict();
   };
@@ -108,7 +108,7 @@ function loadImage(filename, element_id) {
 function loadImageToCanvas(filename, element_id) {
   let _mask = document.getElementById(element_id).getContext('2d');
   let _img = new Image;
-  _img.onload = function() {
+  _img.onload = function () {
     _mask.drawImage(_img, 0, 0, _img.width, _img.height, 0, 0, WIDTH, HEIGHT);
   };
 
@@ -138,7 +138,7 @@ function loadPreset(preset_id) {
 function predict() {
   // Tests if the model is loaded.
   if (segmentationModel == null || segmenter == null ||
-      estimationModel == null || estimator == null) {
+    estimationModel == null || estimator == null) {
     alert('Model is not available!');
     return;
   }
@@ -158,70 +158,74 @@ function predict() {
   predictButton.disabled = true;
 
   // Sets timeout = 0 to force reload the UI.
-  setTimeout(function() {
+  setTimeout(function () {
     const start = Date.now();
     const ctx = resultCanvas.getContext('2d');
     ctx.clearRect(0, 0, resultCanvas.width, resultCanvas.height);
+    const numRuns = 50;
 
     const getPortraitDepth = async () => {
-      const segmentation = await segmenter.segmentPeople(image1);
+      for (let run = 0; run < numRuns; run++) {
+        const segmentation = await segmenter.segmentPeople(image1);
 
-      // Convert the segmentation into a mask to darken the background.
-      const foregroundColor = {r: 0, g: 0, b: 0, a: 0};
-      const backgroundColor = {r: 0, g: 0, b: 0, a: 255};
-      const backgroundDarkeningMask = await bodySegmentation.toBinaryMask(
+        // Convert the segmentation into a mask to darken the background.
+        const foregroundColor = { r: 0, g: 0, b: 0, a: 0 };
+        const backgroundColor = { r: 0, g: 0, b: 0, a: 255 };
+        const backgroundDarkeningMask = await bodySegmentation.toBinaryMask(
           segmentation, foregroundColor, backgroundColor);
-      segmentation.map(
+        segmentation.map(
           singleSegmentation => singleSegmentation.mask.toTensor().then(
-              tensor => tensor.dispose()));
+            tensor => tensor.dispose()));
 
-      const opacity = 1.0;
-      const maskBlurAmount = 0;
-      const flipHorizontal = false;
+        const opacity = 1.0;
+        const maskBlurAmount = 0;
+        const flipHorizontal = false;
 
-      // Draw the mask onto the image on a canvas.  With opacity set to 0.7
-      // and maskBlurAmount set to 3, this will darken the background and blur
-      // the darkened background's edge.
-      await bodySegmentation.drawMask(
+        // Draw the mask onto the image on a canvas.  With opacity set to 0.7
+        // and maskBlurAmount set to 3, this will darken the background and blur
+        // the darkened background's edge.
+        await bodySegmentation.drawMask(
           masked, image1, backgroundDarkeningMask, opacity, maskBlurAmount,
           flipHorizontal);
 
-      const result = await estimator.estimateDepth(
-          image1, {minDepth: config.minDepth, maxDepth: config.maxDepth});
-      const depthMap = await result.toTensor();
+        const result = await estimator.estimateDepth(
+          image1, { minDepth: config.minDepth, maxDepth: config.maxDepth });
+        const depthMap = await result.toTensor();
 
-      tf.tidy(() => {
-        const depthMap3D = tf.expandDims(depthMap, axis = 2);
-        const transformNormalize =
+        tf.tidy(() => {
+          const depthMap3D = tf.expandDims(depthMap, axis = 2);
+          const transformNormalize =
             transformValueRange(0, 1, 0, 255 * 255 * 255);
-        let depth_rescale = tf.add(
+          let depth_rescale = tf.add(
             tf.mul(depthMap3D, transformNormalize.scale),
             transformNormalize.offset);
 
-        let depth_r = tf.floorDiv(depth_rescale, 255.0 * 255.0);
-        let depth_remain =
+          let depth_r = tf.floorDiv(depth_rescale, 255.0 * 255.0);
+          let depth_remain =
             tf.floorDiv(tf.mod(depth_rescale, 255.0 * 255.0), 1.0);
-        let depth_g = tf.floorDiv(depth_remain, 255);
-        let depth_b = tf.floorDiv(tf.mod(depth_remain, 255), 1.0);
+          let depth_g = tf.floorDiv(depth_remain, 255);
+          let depth_b = tf.floorDiv(tf.mod(depth_remain, 255), 1.0);
 
-        let depth_rgb = tf.concat([depth_r, depth_g, depth_b], axis = 2);
+          let depth_rgb = tf.concat([depth_r, depth_g, depth_b], axis = 2);
 
-        // Renders the result on a canvas.
-        const transformBack = transformValueRange(0, 255, 0, 1);
+          // Renders the result on a canvas.
+          const transformBack = transformValueRange(0, 255, 0, 1);
 
-        // Converts back to 0-1.
-        const rgbFinal = tf.clipByValue(
+          // Converts back to 0-1.
+          const rgbFinal = tf.clipByValue(
             tf.add(
-                tf.mul(depth_rgb, transformBack.scale), transformBack.offset),
+              tf.mul(depth_rgb, transformBack.scale), transformBack.offset),
             0, 1);
 
-        tf.browser.toPixels(rgbFinal, resultCanvas);
-      });
+          tf.browser.toPixels(rgbFinal, resultCanvas);
+        });
 
-      depthMap.dispose();
+        depthMap.dispose();
+        console.log('Complete one run!');
+      }
 
       const end = Date.now();
-      const time = end - start;
+      const time = (end - start) / numRuns;
       perf.textContent = `E2E latency: ${time}ms`;
       predictButton.textContent = 'Measure Latency';
       predictButton.disabled = false;
@@ -247,12 +251,12 @@ function transformValueRange(fromMin, fromMax, toMin, toMax) {
   const ToRange = toMax - toMin;
   const scale = ToRange / fromRange;
   const offset = toMin - fromMin * scale;
-  return {scale, offset};
+  return { scale, offset };
 }
 
 function isMobile() {
   return /Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/
-      .test(navigator.userAgent);
+    .test(navigator.userAgent);
 }
 /**
  * Sets up the page.
@@ -272,12 +276,12 @@ async function setupPage() {
 
   try {
     segmentationModel =
-        bodySegmentation.SupportedModels.MediaPipeSelfieSegmentation;
+      bodySegmentation.SupportedModels.MediaPipeSelfieSegmentation;
   } catch (e) {
     predictButton.textContent = 'Error in loading segmentation model.';
   }
   segmenter = await bodySegmentation.createSegmenter(
-      segmentationModel, {runtime: 'tfjs'});
+    segmentationModel, { runtime: 'tfjs' });
 
   try {
     estimationModel = depthEstimation.SupportedModels.ARPortraitDepth;
@@ -297,7 +301,7 @@ async function setupPage() {
   const dropzoneForm = document.getElementById('dropzone');
   let dropzone;
   new Dropzone('#dropzone', {
-    transformFile: function(file, done) {
+    transformFile: function (file, done) {
       dropzone = this;
 
       rightSide.removeChild(dropzoneForm);
@@ -309,7 +313,7 @@ async function setupPage() {
   });
 
   // Allow removal of the uploaded image.
-  deleteUploadButton.addEventListener('click', function() {
+  deleteUploadButton.addEventListener('click', function () {
     uploadedImage.innerHTML = '';
     rightSide.insertBefore(dropzoneForm, uploadedImage);
     dropzone.removeAllFiles(true);
